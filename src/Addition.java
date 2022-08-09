5c6613a367f881b4c53d518fca99a9ca10f0e2c2
naiknirav <naiknirav4@gmail.com>
2022-08-09 10:49:21 -0300

    scanner class added

--- src/Addition.java
+++ src/Addition.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Addition
 {
     public static void main(String[] args) {
         int a=10,b=20,ans;
         ans=a+b;
         System.out.println("Addition is: "+ans);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 1st no:");
+        int x=sc.nextInt();
+        System.out.println("Enter 2nd no:");
+        int y=sc.nextInt();
+        System.out.println("The answer is:");
+        int addition=a+b;
+        System.out.println(ans);
     }
 }
